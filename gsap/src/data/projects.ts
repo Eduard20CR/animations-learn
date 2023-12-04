17c904cf9992ec29f77a5b1ce4eda0646cab7e64
Oscar <oscar.vasquez@havasmagma.com>
2023-12-04 11:23:51 -0600

    updating names, adding threejs example

---
+++ gsap/src/data/projects.ts
@@ -0,0 +1,9 @@
+export const links = [
+  { link: "/", label: "Home" },
+  { link: "/001-scrolltrigger-first-practice/", label: "ScrollTrigger First Practice" },
+  { link: "/002-scrolltrigger-screens-overlaping/", label: "ScrollTrigger Screens Overlaping" },
+  { link: "/003-scrolltrigger-screens-coming/", label: "ScrollTrigger Screens Coming" },
+  { link: "/004-scrolltrigger-horizontal-scroll/", label: "ScrollTrigger Horizontal Scroll" },
+  { link: "/005-scrolltrigger-square-moving/", label: "ScrollTrigger Square Moving" },
+  { link: "/006-threejs-first-practice/", label: "ThreeJS First Practice" },
+];
