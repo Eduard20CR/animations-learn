efcc339f8d62753d7e6b17f3a329376341cd2bfe
Eduard20CR <educhito20@gmail.com>
2024-01-04 16:52:16 -0600

    updating menu

--- gsap/src/data/projects.ts
+++ gsap/src/data/projects.ts
@@ -1,9 +1,27 @@
-export const links = [
-  { link: "/", label: "Home" },
-  { link: "/001-scrolltrigger-first-practice/", label: "ScrollTrigger First Practice" },
-  { link: "/002-scrolltrigger-screens-overlaping/", label: "ScrollTrigger Screens Overlaping" },
-  { link: "/003-scrolltrigger-screens-coming/", label: "ScrollTrigger Screens Coming" },
-  { link: "/004-scrolltrigger-horizontal-scroll/", label: "ScrollTrigger Horizontal Scroll" },
-  { link: "/005-scrolltrigger-square-moving/", label: "ScrollTrigger Square Moving" },
-  { link: "/006-threejs-first-practice/", label: "ThreeJS First Practice" },
+export interface ProjectInfo {
+  link: string;
+  label: string;
+}
+
+export interface GroupInfo {
+  title: string;
+  projects: ProjectInfo[];
+}
+
+// { link: "/", label: "Home" },
+export const links: GroupInfo[] = [
+  {
+    title: "GSAP",
+    projects: [
+      { link: "/001-scrolltrigger-first-practice/", label: "Scroll Animation (ScrollTrigger)" },
+      { link: "/002-scrolltrigger-screens-overlaping/", label: "Screens Overlaping (ScrollTrigger)" },
+      { link: "/003-scrolltrigger-screens-coming/", label: "Screens Coming (ScrollTrigger)" },
+      { link: "/004-scrolltrigger-horizontal-scroll/", label: "Horizontal Scroll (ScrollTrigger)" },
+      { link: "/005-scrolltrigger-square-moving/", label: "Square Moving (ScrollTrigger)" },
+    ],
+  },
+  {
+    title: "ThreeJS",
+    projects: [{ link: "/006-threejs-first-practice/", label: "ThreeJS First Practice" }],
+  },
 ];
--- gsap/tailwind.config.mjs
+++ gsap/tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#C5EDAC",
+          secondary: "#6CCFF6",
+          neutral: "#e5e7eb",
+          gray: "#757780",
+          white: "#FFFFFC",
+          black: "#001011",
+        },
+      },
+    ],
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+};
